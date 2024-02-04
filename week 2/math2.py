2df55fede3e4b991b0c89a93dfcb09a4a9f80ab0
Fatima Oliveira <Fatima.21.00@hotmail.com>
2024-02-04 18:48:23 +0000

    week 2 exercises

---
+++ week 2/math2.py
@@ -0,0 +1,8 @@
+#Math 2
+#Just other calculations
+#Author: Fatima
+
+answer = 21 - 4
+print(answer)
+
+print(2==3)
---
+++ week 2/nameAndAge.py
@@ -0,0 +1,8 @@
+#Name and age
+#Program that will say your name and age
+#Author: Fatima
+
+
+name=input ("Enter your name: ")
+number = int(input("Please enter a number: "))
+print (f"Hello {name}, \t your age is {number}")
