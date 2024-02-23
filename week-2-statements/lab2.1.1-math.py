a24709f1ce4742767d96c375fa1ac1e8861cc9c3
Fatima Oliveira <Fatima.21.00@hotmail.com>
2024-02-23 18:24:09 +0000

    updated

--- week-2-statements/lab2.1.1-math.py
+++ week-2-statements/lab2.1.1-math.py
@@ -11,11 +11,11 @@ print(5 - 8)
 print(21 * 55)
 print(21 / 4)
 print(21 // 4)
-print(%(21/4))
+print (21%4)
 print(2 ** 8)
 
 print(2==3)
 print(2 != 3)
+
 print(2+
-      ...4
-      ...)
+      4)
