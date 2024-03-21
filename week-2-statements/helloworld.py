a96dddb2db3245e9f2c959876a38f747c21fa83c
Fatima Oliveira <Fatima.21.00@hotmail.com>
2024-03-21 17:04:59 +0000

    delete files from GitHub

--- week-2-statements/helloworld.py
+++
@@ -1,5 +0,0 @@
-# Hello world!
-# Program that prints Hello world!
-# Author: Fatima
-
-print ("Hello World!")
--- week-2-statements/multiply.py
+++
@@ -1,6 +0,0 @@
-#Multiply
-#Just a multiplication
-#Author: Fatima
-
-answer= 111 * 555
-print (answer)
