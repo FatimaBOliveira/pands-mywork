c298fdc181c0ffdc816cb19a1c76f0f8f34669ba
Fatima Oliveira <Fatima.21.00@hotmail.com>
2024-03-21 17:43:53 +0000

    week 9 lab

---
+++ week-9-errors/lab9.2-Fib.py
@@ -0,0 +1,7 @@
+# Fib
+# Program that ask user for a number and will return the list of Fibonacci sequence
+# Author: Fatima Oliveira
+
+import lab9_1_myFunctions as lab
+nTimes = int(input('how many:'))
+print(lab.fibonacci(nTimes))
